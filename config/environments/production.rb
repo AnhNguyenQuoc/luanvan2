b6dda3bd28a71838c5aecb872c5e55f4ecff8c00
Anh Nguyen <nguyenquocanh121296@gmail.com>
2018-11-24 16:30:18 +0000

    upload to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,15 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+  config.action_mailer.smtp_settings = {
+    :user_name => 'AnhNguyen1212',
+    :password => 'dancesport123',
+    :domain => 'localhost:3000',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new

e677435d1c4767217e2f8f497ba311b18e0d4f61
Anh Nguyen <nguyenquocanh121296@gmail.com>
2018-12-07 17:29:33 +0700

    Add dev

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
   # Do not eager load code on boot.
   config.eager_load = false
   
@@ -35,10 +37,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'http' }
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 
-  config.cache_classes = true
-  config.serve_static_assets = true
-  config.assets.compile = false
-  config.assets.digest = true
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
